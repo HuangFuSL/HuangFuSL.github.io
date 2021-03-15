bddbfcc87fa1721519bf65211db40a7fa2896ba3
HuangFuSL <56627153+HuangFuSL@users.noreply.github.com>
2021-03-15 21:07:28 +0800

    Removed built pages from the main branch

--- build.py
+++ build.py
@@ -17,16 +17,6 @@ def check_doc(cwd: str):
 
 
 SOURCE_DIR = "."
-HTML_DIR = os.path.join("..", "HuangFuSL.github.io")
-
-COMMAND_LIST = {
-    'build': 'mkdocs build -d built --no-directory-urls',
-    'check': 'git status',
-    'push': 'git push',
-    'pull': 'git pull',
-    'add': 'git add --all',
-    'commit': 'git commit -a --message="%s"'
-}
 
 if __name__ == "__main__":
     os.chdir(SOURCE_DIR)
@@ -41,22 +31,10 @@ if __name__ == "__main__":
     # if the working tree is clean,
     # no rebuild and commit required.
     src = os.getcwd()
-    subprocess.run(COMMAND_LIST['pull'], cwd=src)
-    subprocess.run(COMMAND_LIST['build'], cwd=src)
-    ret2 = subprocess.run(
-        COMMAND_LIST['check'], text=True, capture_output=True, cwd=src)
-    if 'nothing to commit' not in ret2.stdout:
-        subprocess.run(COMMAND_LIST['add'], cwd=src)
-        subprocess.run(
-            COMMAND_LIST['commit'] % (
-                "Update on " +
-                time.strftime(
-                    "%Y/%m/%d %H:%M:%S",
-                    time.localtime(time.time())
-                ),
-            ),
-            cwd=src
-        )
-        subprocess.run(COMMAND_LIST['push'], cwd=src)
-    else:
-        print("HTML files not updated.")
+    msg = "Update on " + \
+        time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time.time()))
+    subprocess.run(
+        'mkdocs gh-deploy -d built --message "%s" --no-directory-urls' % (msg, ),
+        cwd=src
+    )
+    
--- built/latex/index.md
+++
@@ -1,7 +0,0 @@
-# LaTeX
-
-记录LaTeX的使用。
-
-* [LaTeX数学公式](cheatsheet.md)
-* [LaTeX语句环境](environ.md)
-* [使用LaTeX创建幻灯片](slides.md)
