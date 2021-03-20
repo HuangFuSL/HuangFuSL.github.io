e38a68558decb87081d478fb80272aa6e90793fe
HuangFuSL <56627153+HuangFuSL@users.noreply.github.com>
2021-03-20 14:02:09 +0000

    "Update on 2021/03/20 14:02:09"

--- build.py
+++ build.py
@@ -35,16 +35,16 @@ if __name__ == "__main__":
         time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time.time()))
     if len(sys.argv) > 1 and sys.argv[1] == "github-actions":
         subprocess.run(
-            'mkdocs gh-deploy -d built --message "%s"' % (msg, ),
+            'mkdocs gh-deploy -d built --message'.split() + [msg],
             cwd=src, shell=True
         )
     else:
-        subprocess.run('git add --all', cwd=src)
+        subprocess.run('git add --all'.split(), cwd=src)
         subprocess.run(
-            'git commit -a --message="%s"' % (msg, ),
+            ['git', 'commit', '-a', '--message="%s"' % (msg, )],
             cwd=src
         )
         subprocess.run(
-            'git push',
+            'git push'.split(),
             cwd=src
         )
