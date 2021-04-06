6e733f263c770c55f3c48c4467a719d96a1977c9
HuangFuSL <56627153+HuangFuSL@users.noreply.github.com>
2021-04-06 22:52:09 +0800

    Modified build process and added tex support

--- build.py
+++ build.py
@@ -1,65 +1,24 @@
 import time
 import os
 import subprocess
-import doctest
-import sys
+import convert
 
 
-def check_doc(cwd: str):
-    for _ in os.listdir(cwd):
-        curPath = os.path.join(cwd, _)
-        if os.path.isdir(curPath):
-            check_doc(curPath)
-        elif os.path.isfile(curPath):
-            if _[-2:] == "md" and "pyguide" not in _:
-                doctest.testfile(
-                    curPath, verbose=True, optionflags=doctest.ELLIPSIS)
-
-def apply_token(token: str):
-    new = os.path.join("overrides", "main.html")
-    old = os.path.join("overrides", "_main.html")
-    _output = open(new, "w", encoding='utf-8')
-    _input = open(old, "r", encoding="utf-8")
-    for line in _input.readlines():
-        if "clientSecret" in line:
-            line = line % (token, )
-        _output.write(line)
-    _input.close()
-    _output.close()
-    
-
-SOURCE_DIR = "."
 
 if __name__ == "__main__":
-    os.chdir(SOURCE_DIR)
-
-    if "GITALK_SECRET" in os.environ:
-        apply_token(os.environ["GITALK_SECRET"])
-
-    try:
-        check_doc(SOURCE_DIR)
-    except:
-        print("doctest failed")
-        sys.exit(-1)
-
+    convert.tex2svg(os.getcwd())
     # check the status of the source
     # if the working tree is clean,
     # no rebuild and commit required.
     src = os.getcwd()
     msg = "Update on " + \
         time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(time.time() + 3600 * 8))
-    if len(sys.argv) > 1 and sys.argv[1] == "github-actions":
-        subprocess.run(
-            'mkdocs gh-deploy -d built --message "%s"' % (msg, ),
-            cwd=src, shell=True
-        )
-    else:
-        subprocess.run('git add --all'.split(), cwd=src)
-        subprocess.run(
-            ['git', 'commit', '-a', '--message=%s' % (msg, )],
-            cwd=src
-        )
-        subprocess.run(
-            'git push'.split(),
-            cwd=src
-        )
+    subprocess.run('git add --all'.split(), cwd=src)
+    subprocess.run(
+        ['git', 'commit', '-a', '--message=%s' % (msg, )],
+        cwd=src
+    )
+    subprocess.run(
+        'git push'.split(),
+        cwd=src
+    )
---
+++ ci/deploy.py
@@ -0,0 +1,35 @@
+import time
+import os
+import subprocess
+import sys
+
+
+def apply_token(token: str):
+    new = os.path.join("overrides", "main.html")
+    old = os.path.join("overrides", "_main.html")
+    _output = open(new, "w", encoding='utf-8')
+    _input = open(old, "r", encoding="utf-8")
+    for line in _input.readlines():
+        if "clientSecret" in line:
+            line = line % (token, )
+        _output.write(line)
+    _input.close()
+    _output.close()
+
+
+SOURCE_DIR = "."
+
+if __name__ == "__main__":
+    os.chdir(SOURCE_DIR)
+    if "CI" not in os.environ or not os.environ["CI"]:
+        print("This script is designed for CI execution.")
+        sys.exit()
+    if "GITALK_SECRET" in os.environ:
+        apply_token(os.environ["GITALK_SECRET"])
+
+    src = os.getcwd()
+    msg = "Update on " + time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(time.time() + 3600 * 8))
+    subprocess.run(
+        'mkdocs gh-deploy -d built --message "%s"' % (msg, ),
+        cwd=src, shell=True
+    )
