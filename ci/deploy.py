9e898f5791f7725e2833c44770446705f56a8169
HuangFuSL <huangfusl@outlook.com>
2022-11-04 23:57:22 +0800

    Removed gitalk configuration

--- ci/deploy.py
+++ ci/deploy.py
@@ -12,22 +12,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dry-run", action="store_true", help="Do not actually deploy")
 
 
-def apply_token(id: str, token: str):
-    new = os.path.join("overrides", "main.html")
-    old = os.path.join("overrides", "_main.html")
-    _output = open(new, "w", encoding='utf-8')
-    _input = open(old, "r", encoding="utf-8")
-    for line in _input.readlines():
-        if "clientSecret" in line:
-            line = line % (token, )
-        if "clientID" in line:
-            line = line % (id, )
-        _output.write(line)
-    print("Token applied to {}".format(new))
-    _input.close()
-    _output.close()
-
-
 SOURCE_DIR = "."
 
 if __name__ == "__main__":
@@ -37,8 +21,6 @@ if __name__ == "__main__":
     if ("CI" not in os.environ or not os.environ["CI"]) and not dry_run:
         print("This script is designed for CI execution.")
         sys.exit()
-    if "GITALK_SECRET" in os.environ and "GITALK_ID" in os.environ:
-        apply_token(os.environ["GITALK_ID"], os.environ["GITALK_SECRET"])
 
     src = os.getcwd()
     convert.tex2svg(src)
