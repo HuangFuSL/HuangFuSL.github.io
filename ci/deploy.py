4486fcf6e3972784048db959243eb02e35627971
HuangFuSL <huangfusl@outlook.com>
2022-11-05 16:48:44 +0800

    Formatted code according to pylint

--- ci/deploy.py
+++ ci/deploy.py
@@ -9,33 +9,33 @@ import convert
 import tsinghua
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dry-run", action="store_true", help="Do not actually deploy")
+parser.add_argument('--dry-run', action='store_true')
 
 
-SOURCE_DIR = "."
+SOURCE_DIR = '.'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     ns = parser.parse_args()
     dry_run = ns.dry_run
     os.chdir(SOURCE_DIR)
-    if ("CI" not in os.environ or not os.environ["CI"]) and not dry_run:
-        print("This script is designed for CI execution.")
+    if ('CI' not in os.environ or not os.environ['CI']) and not dry_run:
+        print('This script is designed for CI execution.')
         sys.exit()
 
     src = os.getcwd()
     convert.tex2svg(src)
     bootstrap.get_icon()
     tsinghua.build_icon()
-    msg = "Update on " + \
-        time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(time.time() + 3600 * 8))
+    msg = 'Update on ' + \
+        time.strftime('%Y/%m/%d %H:%M:%S', time.gmtime(time.time() + 3600 * 8))
     subprocess.run(
         'mkdocs build -d build',
-        cwd=src, shell=True
+        cwd=src, shell=True, check=True
     )
     if not dry_run:
         subprocess.run(
-            'mkdocs gh-deploy -d build --message "%s"' % (msg, ),
-            cwd=src, shell=True
+            f'mkdocs gh-deploy -d build --message "{msg}"',
+            cwd=src, shell=True, check=True
         )
     else:
-        print("Dry run, not deploying.")
+        print('Dry run, not deploying.')
--- main/latex.py
+++ main/latex.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 from xml.etree import ElementTree
 
 
-def latex_image(filename: str, alt: str = "", width: str | None = None, page: int | None = None):
+def latex_image(
+    filename: str, alt: str = '',
+    width: str | None = None, page: int | None = None
+):
 
     img = ElementTree.Element('img')
     if page is None:
-        filename = "{0}.svg".format(filename[:-4])
+        filename = f'{filename[:-4]}.svg'
     else:
-        filename = "{0}-{1}.svg".format(filename[:-4])
+        filename = f'{filename[:-4]}-{page}.svg'
     img.set('src', '../' + filename)
     img.set('alt', alt)
     img.set('class', 'latex')
