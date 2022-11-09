e0c6b03c3de91c69f46153d585df4831e059de51
HuangFuSL <huangfusl@outlook.com>
2022-11-09 11:56:02 +0800

    Fixed doctest failure

--- ci/test_code_block.py
+++ ci/test_code_block.py
@@ -1,6 +1,13 @@
 import os
 import doctest
 
+TEST_ARGS = {
+    'module_relative': False,
+    'optionflags': doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL,
+    'raise_on_error': True,
+    'verbose': True
+}
+
 def check_doc(cwd: str):
     for _ in os.listdir(cwd):
         curPath = os.path.join(cwd, _)
@@ -8,11 +15,7 @@ def check_doc(cwd: str):
             check_doc(curPath)
         elif os.path.isfile(curPath):
             if _[-2:] == "md" and "pyguide" not in _:
-                doctest.testfile(
-                    curPath,
-                    verbose=True, raise_on_error=True,
-                    optionflags=doctest.ELLIPSIS
-                )
+                doctest.testfile(curPath,**TEST_ARGS)
 
 if __name__ == "__main__":
     src = os.getcwd()
