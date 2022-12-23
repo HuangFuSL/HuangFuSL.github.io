840939b2a1b8bb5408990f1e0e2ba47f5e7183fa
HuangFuSL <huangfusl@outlook.com>
2022-12-23 12:42:17 +0800

    Fixed \boldsymbol not found

--- docs/js/mathjax.js
+++ docs/js/mathjax.js
@@ -3,14 +3,16 @@ window.MathJax = {
         inlineMath: [['\\(', '\\)']],
         displayMath: [['\\[', '\\]']],
         processEscapes: true,
-        processEnvironments: true
+        processEnvironments: true,
+        packages: { '[+]': ['boldsymbol'] }
     },
     chtml: { scale: 0.8 },
     svg: { scale: 0.8 },
     options: {
         ignoreHtmlClass: '.*|',
         processHtmlClass: 'arithmatex'
-    }
+    },
+    loader: { load: ['[tex]/boldsymbol'] },
 };
 
 document$.subscribe(() => {
