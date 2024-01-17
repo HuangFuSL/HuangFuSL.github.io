3e74d14b659d88fa7aa7f0d9318c2501077eccf4
HuangFuSL <huangfusl@outlook.com>
2024-01-17 17:11:00 +0800

    Fix: broken links

--- main/coding.py
+++ main/coding.py
@@ -4,8 +4,9 @@ def read_code_from_file(file_name: str, language: str | None = None) -> str:
     ''' Read from file, return a link and code block '''
     if language is None:
         language = file_name.split('.')[-1]
+    file_url = file_name.removeprefix('docs')
     suffix_mapping = { 'py': 'python', 'rs': 'rust' }
-    link = f'[:bootstrap-cloud-download: Download source code]({file_name})\n\n'
+    link = f'[:bootstrap-cloud-download: Download source code]({file_url})\n\n'
     code_block_prefix = f'```{suffix_mapping.get(language, str())}\n'
     with open(file_name, 'r') as f:
         code_block_body = f.read()
